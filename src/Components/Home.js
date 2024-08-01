a8bc67dd06dafb3163a0b3f4cbf52df1a8762dc8
vaishnavinp01 <vaishnavinp01@gmail.com>
2024-08-01 15:17:53 +0530

    done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,11 +18,11 @@ const Home = () => {
           technologies like HTML, CSS, JavaScript, ExpressJs, ReactJs, MongoDB,
           NodeJs.
         </p>
-        <a href={Vaishnavi_Resume} download="Vaishnawi_Resume" target="_blank" rel="noreferrer" className="home-anchor">
+        <div className={`home-img ${theme}-mode`}></div>
+      </div>
+      <a href={Vaishnavi_Resume} download="Vaishnawi_Resume" target="_blank" rel="noreferrer" className="home-anchor">
           <button className={`home-btn ${theme}-mode`}>Download Resume</button>
         </a>
-      </div>
-      <div className={`home-img ${theme}-mode`}></div>
     </section>
   );
 };
